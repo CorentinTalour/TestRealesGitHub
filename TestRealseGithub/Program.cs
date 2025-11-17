7bc466c31b27189b59bfa5d5e795b0b1b88d7e3b
CorentinTalour <azrod003@gmail.com>
2025-11-17 20:36:05 +0100

    qsdfgd

--- TestRealseGithub/Program.cs
+++ TestRealseGithub/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Worlfsd!");
+app.MapGet("/", () => "Hello Worlfqsdfvxbsd!");
 
 app.Run();
